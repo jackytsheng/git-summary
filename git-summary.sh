03d33c4e2859e1fe28cb172e28d20d74ba0f4872
jackytsheng <jackytsheng@gmail.com>
2022-12-15 23:25:14 +1100

    Modify the script

--- git-summary.sh
+++ git-summary.sh
@@ -1,23 +1,57 @@
 #!/bin/bash
 
-PrintWidth=25
+sflag=
+bflag=
+aflag=
+args=()
+
+while getopts "s:b:a:" flag
+do
+case $flag in
+    s)    sflag=1
+          since="$OPTARG";;
+    b)    bflag=1
+          before="$OPTARG";;
+    a)    aflag=1
+          author="$OPTARG";;
+    ?)   printf 'Usage: %s: [-s <date>] [-b <date>] [-a <value>] args\n' $0
+          exit 2;;
+    esac
+done
+shift $((OPTIND -1))
+
+if [ ! -z "$sflag" ]; then
+	args+=('--since="$since"')
+	echo "flag --since $since specified"
+fi
+if [ ! -z "$bflag" ]; then
+	args+=('--before="$before"')
+	echo "flag --before $before specified"
+fi
+if [ ! -z "$aflag" ]; then
+	args+=('--author="$author"')
+	echo "flag --author $author specified"
+fi
+
+
 for dir in */; do
-    echo -n "--------"
-    printf "%5s%${PrintWidth}s%5s" "" "${dir///}" ""
-    echo "--------"
-    echo ""
-    cd $dir 
-    git shortlog -sne --since="12 Dec 2021" --before="12 Dec 2022" --author=jiajin 
-    git log --shortstat --since="12 Dec 2021" --before="12 Dec 2022" --author=jiajin| awk '/^ [0-9]|^A/ {
-    	if($1=="Author:") 
+	git -C $dir rev-parse 2>/dev/null;
+if [[ $? -eq 0 ]]
+	then
+		cd $dir 
+		echo "---------------${dir///}"
+		git shortlog -sne ${args[@]}
+		git log --shortstat ${args[@]}| awk '/^ [0-9]|^A/ {
+	if($1=="Author:") 
 		c += 1;
 	else 
 		f += $1; 
 		i += $4; 
 		d += $6 
-	} END {
- 		printf("%6d  commits, %d  files changed, %d  insertions(+), %d  deletions(-) \n",c, f, i, d) 
-	}'
-    echo ""
-    cd .. 
+} END {
+	printf("%6d  commits, %d  files changed, %d  insertions(+), %d  deletions(-) \n",c, f, i, d) 
+}'
+		cd .. 
+		echo ""
+fi
 done
